6d81c88d06764b977dc7859ae8664eb559ba18ef
SanjeevSinjali <tdkzbiru@gmail.com>
2024-02-25 21:50:35 +0545

    Project init

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+ 
+root = Tk()
+ 
+root.title("Welcome to bussewa")
+root.geometry('350x200')
+ 
+lbl = Label(root, text = "Are you a ready to book a ride?")
+lbl.grid()
+ 
+root.mainloop()
